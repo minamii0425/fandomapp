32eb55d2ea516f223626c09b88ff7ac10665f878
Minami Hoshi <AHA04293@jp.ibm.com>
2022-12-22 20:58:21 +0900

    base_urlを変更

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false, // コンポーネントが２回レンダリングされる仕様となっているのでfalseに
   swcMinify: true,
   env: {
-    BASE_URL: process.env.BASE_URL,
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 };
 
