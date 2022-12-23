885d0380d7f055a86f402e7e3e80c0af51d75b65
Minami Hoshi <AHA04293@jp.ibm.com>
2022-12-23 19:20:52 +0900

    CORS許可

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@ const nextConfig = {
   // env: {
   //   BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   // },
+  // CORS許可設定
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
